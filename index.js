a0313d705c5633742f27b5c503a800c568e67d73
ray omego <ray.omego@student.moringaschool.com>
2022-08-16 17:49:17 +0300

    done

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a) {
+    return a++;
+}
+
+function subtract(a){
+    return a--;
+}
+
+function divide(a) {
+    return a /= 4;
+}
+
+function multiply(a,b){
+    return a*b
+    }
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b; 
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function increment(n){
+    return n += 1;
+}
+
+function decrement(n){
+    return n -= 1;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+} 
